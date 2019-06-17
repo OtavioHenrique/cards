221a778e23f57eb3e96e2049fcee9fa24cb94763
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-06-17 00:27:47 -0300

    Starting project

---
+++ lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    :world
+  end
+end
