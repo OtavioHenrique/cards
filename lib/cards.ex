bb6f7bb30e2836de16a51c9326fb2820d9810c45
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-07-07 21:48:51 -0300

    Create hand method

--- lib/cards.ex
+++ lib/cards.ex
@@ -35,4 +35,10 @@ defmodule Cards do
       { :error, _reason } -> "That file doesn't exist"
     end
   end
+
+  def create_hand(hand_size) do
+    create_deck
+    |> shuffle
+    |> deal(hand_size)
+  end
 end
